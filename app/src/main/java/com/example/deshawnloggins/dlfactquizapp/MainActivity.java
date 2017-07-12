973a81be8dce066163013009812915299898fc0e
thekidwhoknows <deshawn.loggins@hotmail.com>
2017-07-12 06:05:46 -0500

    Revised

--- app/src/main/java/com/example/deshawnloggins/dlfactquizapp/MainActivity.java
+++ app/src/main/java/com/example/deshawnloggins/dlfactquizapp/MainActivity.java
@@ -17,32 +17,27 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 //**************************************************************************************************
 // This is variable is used to keep track of the total from all the other methods.
 //**************************************************************************************************
     int total = 0;
-
 //**************************************************************************************************
 // This method is suppose to submit the quiz and report the total the user got right out of 10.
 //**************************************************************************************************
     public void submitQuiz(View view)
     {
-        anythingIsTheCorrentAnswer();
         String gotName;
-
+        sleepIsTheOnlyAnswer();
+        favoriteAnime();
+        favoriteGames();
         EditText name = (EditText) findViewById(R.id.players_name);
        gotName = name.getText().toString();
-
         Context context = getApplicationContext();
         CharSequence text =gotName + " Your Total is: " + total + " out of 10" ;
         int duration = Toast.LENGTH_LONG;
-
         Toast toast = makeText(context, text, duration);
         toast.show();
-
     }
-
 //**************************************************************************************************
 // This method is used to to check which button is checked and send back a true or false and report
 // the total if the correct answer is chosen.
@@ -50,26 +45,21 @@ public class MainActivity extends AppCompatActivity
     public void hobbysButtonClicked(View view)
     {
         boolean checked = ((RadioButton) view).isChecked();
-
         switch(view.getId())
         {
             case R.id.music_button:
                 if (checked)
-
                     break;
+
             case R.id.running_button:
                 if (checked)
-
                     total+= 1;
                 break;
             case R.id.progamming_button:
                 if (checked)
-
                     break;
         }
-
     }
-
 //**************************************************************************************************
 //This function is used to get a true or false to see if a button is checked but also the correct
 //Answer adds 1 to the total which is the global variable.
@@ -77,137 +67,76 @@ public class MainActivity extends AppCompatActivity
     public void favoriteFoodButtonClicked(View view)
     {
         boolean checked = ((RadioButton) view).isChecked();
-
         switch(view.getId())
         {
             case R.id.laptop_button:
                 if (checked)
-
                     break;
             case R.id.book_button:
                 if (checked)
-
                     total+= 1;
                 break;
             case R.id.Toast_button:
                 if (checked)
-
                     break;
         }
-
     }
-
 //**************************************************************************************************
 //This method sends back a + 1 into total if checked comes back true.
 //**************************************************************************************************
-    public void bokuNoHero(View view)
+    public void favoriteAnime()
 {
-    boolean hero;
-
     CheckBox bokuHero = (CheckBox) findViewById(R.id.Hero_Academia_checkBox);
-    hero = bokuHero.isChecked();
-
-    if(hero == true)
-    {
-        total+= 1;
-    }
-
-}
-
-//**************************************************************************************************
-//This method sends back a +1 into total if checked comes back true.
-//**************************************************************************************************
-    public void stride(View view)
-{
-    boolean strideAnime;
-
+    boolean hero = bokuHero.isChecked();
     CheckBox stride = (CheckBox)findViewById(R.id.Stride_CheckBox);
-    strideAnime =  stride.isChecked();
-
-    if(strideAnime == true)
+    boolean strideAnime =  stride.isChecked();
+    CheckBox sao = (CheckBox)findViewById(R.id.SAO_CheckBox);
+    boolean swordArtOnline =  sao.isChecked();
+    if(hero)
     {
         total+= 1;
     }
-}
-
-//**************************************************************************************************
-//This method sends back a return statment because it isn't mean to do anything.
-//**************************************************************************************************
-    public void sAO(View view)
-    {
-        boolean SwordArt;
-        CheckBox sao = (CheckBox)findViewById(R.id.SAO_CheckBox);
-        SwordArt =  sao.isChecked();
-
-        if(SwordArt == true)
+        if (strideAnime)
         {
-           return;
-
+            total += 1;
         }
 
-    }
-
+            if (swordArtOnline)
+            {
+                total -= 0;
+            }
+}
 //**************************************************************************************************
 //This method sends back a +1 into total if the checkbox is clicked.
 //**************************************************************************************************
-    public void overWatch(View view)
+    public void favoriteGames()
     {
-        boolean overwatch;
-        CheckBox overWatchGame = (CheckBox)findViewById(R.id.Overwatch_checkBox);
-        overwatch =  overWatchGame.isChecked();
+        CheckBox overWatchGame = (CheckBox) findViewById(R.id.Overwatch_checkBox);
+        boolean overwatch = overWatchGame.isChecked();
+        CheckBox lifeIsStrangeGame = (CheckBox) findViewById(R.id.Life_Strange_Checkbox);
+        boolean lifestrange = lifeIsStrangeGame.isChecked();
+        CheckBox left4DeadGame = (CheckBox) findViewById(R.id.Left_4_Dead_checkBox);
+        boolean left4 = left4DeadGame.isChecked();
+        CheckBox ultimateDespair = (CheckBox) findViewById(R.id.Ultimate_Despair_checkBox);
+        boolean despair = ultimateDespair.isChecked();
 
-        if(overwatch == true)
+        if (overwatch)
         {
-            total+= 1;
+            total += 1;
         }
-
-    }
-
-//**************************************************************************************************
-//This method sends back a +1 into the total if the checkbox is checked.
-//**************************************************************************************************
-    public void lifeIsStrange(View view)
-    {
-        boolean lifestrange;
-        CheckBox lifeIsStrangeGame = (CheckBox)findViewById(R.id.Life_Strange_Checkbox);
-        lifestrange =  lifeIsStrangeGame.isChecked();
-
-        if(lifestrange == true)
+        if(lifestrange)
         {
-            total+= 1;
+            total+=1;
         }
-    }
-
-//**************************************************************************************************
-//This method sends back a +1 into total if the checkbox is checked.
-//**************************************************************************************************
-    public void left4Dead(View view)
-    {
-        boolean left4;
-        CheckBox left4DeadGame = (CheckBox)findViewById(R.id.Left_4_Dead_checkBox);
-        left4 =  left4DeadGame.isChecked();
-
-        if(left4 == true)
+        if(left4)
         {
-            total+= 1;
+            total+=1;
+        }
+        if(despair)
+        {
+            total+=1;
         }
     }
-
-//**************************************************************************************************
-//This method sends a +1 into total if checked.
-//**************************************************************************************************
-    public void ultimateDespair(View view)
-    {
-        boolean despair;
-        CheckBox ultimateDespair = (CheckBox)findViewById(R.id.Ultimate_Despair_checkBox);
-        despair =  ultimateDespair.isChecked();
-
-        if(despair == true)
-        {
-            total+= 1;
-        }
-    }
-
 //**************************************************************************************************
 //This method is meant to reset the score, which is the total, but I understand that it doesn't
 //rest the buttonClicks themesleves, just the total.
@@ -215,15 +144,12 @@ public class MainActivity extends AppCompatActivity
     public void resetButton(View view)
     {
         total = 0;
-
         Context context = getApplicationContext();
         CharSequence text ="Your Total is: " + total + " out of 10" ;
         int duration = Toast.LENGTH_SHORT;
         Toast toStartFromZero = Toast.makeText(context, text, duration);
         toStartFromZero.show();
-
     }
-
 //**************************************************************************************************
 //This method is used as a display text into a toast to give the user a a little more information
 //About myself and to motivate them a little more in there own lives.
@@ -233,29 +159,30 @@ public class MainActivity extends AppCompatActivity
         Context context = getApplicationContext();
         CharSequence text ="One of my favorite qoutes is:'I don't think I'll learn anything if I don't try to do something about it on my own.' " ;
         int duration = Toast.LENGTH_LONG;
-
         Toast extrasInfo = makeText(context, text, duration);
         extrasInfo.show();
-
     }
-
 //**************************************************************************************************
 //This is an anything goes method so there is no wrong answer.
 //**************************************************************************************************
-    public void anythingIsTheCorrentAnswer()
+    public void sleepIsTheOnlyAnswer()
     {
-        String answerForMoo;
-
-        EditText Moo = (EditText) findViewById(R.id.Anything_goes);
-        answerForMoo = Moo.getText().toString();
-
-       if(answerForMoo != null)
-
+        EditText Moo = (EditText) findViewById(R.id.Favorite_Thing_IN_World);
+       String answerForMoo = Moo.getText().toString();
+       String rest ="sleep";
+       String rest2 ="Sleep";
+       if(answerForMoo.equals(rest) || answerForMoo.equals(rest2))
        {
            total += 2;
        }
-
+       else
+       {
+           Context context = getApplicationContext();
+           CharSequence text = "Your answer for Question 1 is wrong. Think, what do you need 8 hour of everyday ? " ;
+           int duration = Toast.LENGTH_LONG;
+           Toast toast = makeText(context, text, duration);
+           toast.show();
+       }
     }
 //**************************************************************************************************
-
 }
